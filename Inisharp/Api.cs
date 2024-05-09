5c6ebd4c315e02fb14563905d280373d3a71faf7
oatgh2 <gabriel@mwsoftware.com.br>
2024-05-08 22:10:08 -0300

    Adicionar arquivos de projeto.

---
+++ Inisharp/Api.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inisharp.WinApi
+{
+  internal static class Api
+  {
+    [DllImport("kernel32")]
+    internal static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+    [DllImport("kernel32")]
+    internal static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder reVal, int size, string filePath);  }
+}
---
+++ Inisharp/Ini.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using Inisharp.WinApi;
+namespace Inisharp.Ini
+{
+  public class Ini
+  {
+    private string _iniPath;
+
+    public Ini(string iniPath)
+    {
+      _iniPath = iniPath;
+    }
+
+    public string ReadValue(string section, string key, string value)
+    {
+      StringBuilder temp = new StringBuilder(0xff);
+      int i = Api.GetPrivateProfileString(section, key, value, temp, 0xff, this._iniPath);
+      return temp.ToString();
+    }
+
+    public void WriteValue(string section, string key, string value)
+    {
+      Api.WritePrivateProfileString(section, key, value, this._iniPath);
+    }
+  }
+}
